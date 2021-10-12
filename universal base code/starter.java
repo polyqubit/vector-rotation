5de9aa7f13f448a9bf730474ede5249608654b38
polyqubit <polyumbrail@gmail.com>
2021-10-12 12:41:22 -0700

    wikipedia is lie

---
+++ universal base code/starter.java
@@ -0,0 +1,43 @@
+import pkg.*;
+import java.util.ArrayList;
+import java.lang.Math;
+
+public class starter implements InputControl, InputKeyControl {
+	static Line vector;
+	
+	public static void main(String args[]) {
+		// please leave following line alone, necessary for keyboard/mouse input
+		KeyController kC = new KeyController(Canvas.getInstance(),new starter());
+		MouseController mC = new MouseController(Canvas.getInstance(),new starter());
+		
+		double pointX = 300;
+		double pointY = 20;
+		double tempX,tempY;
+		double angle = 1;
+		vector = new Line(300,300, pointX,pointY);
+		vector.draw();
+		Ellipse origin = new Ellipse(290,290, 20,20);
+		Color oC = new Color(255,0,0);
+		origin.setColor(oC);
+		origin.fill();
+		while(true) {
+			tempX = (pointX * Math.cos(angle)) - (pointY * Math.sin(angle));
+			tempY = (pointX * Math.sin(angle)) + (pointY * Math.cos(angle));
+			pointX = tempX;
+			pointY = tempY;
+			vector = new Line(300,300, pointX,pointY);
+			vector.draw();
+			Canvas.pause(10);
+		}
+	}
+
+	public void onMouseClick(double x, double y) {
+		// enter code here
+
+	}
+
+	public void keyPress(String s) {
+		// enter code here
+
+	}
+}
